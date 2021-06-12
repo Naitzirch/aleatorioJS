4d9d9d9ce1155a512ebff780c69e1f5a5dc3be70
BuildTools <unconfigured@null.spigotmc.org>
2021-06-12 23:23:23 +0000

    fixed mistake

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(443, function(){
+var server = app.listen(4000, function(){
   console.log('listening to requests on port 4000');
 });
 
