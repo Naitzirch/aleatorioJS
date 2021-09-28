d09bc08e1f6661bd997d0fd2cbdc0a7cd848f374
Naitzirch <christian.ca.martens@gmail.com>
2021-09-28 16:21:34 +0200

    Added stats page

---
+++ public/stats/stats.js
@@ -0,0 +1,16 @@
+// Make connection
+var development = false;
+var socket;
+if (development){
+  socket = io.connect('http://localhost:4000');
+}
+else {
+  socket = io.connect('https://aleatorio.net', { secure: true, port: 443});
+}
+
+var nbUsers = document.getElementById('nbUsers');
+
+socket.on('updateUsers', function(data){
+  console.log(data);
+  nbUsers.innerText = data - 1;
+});
